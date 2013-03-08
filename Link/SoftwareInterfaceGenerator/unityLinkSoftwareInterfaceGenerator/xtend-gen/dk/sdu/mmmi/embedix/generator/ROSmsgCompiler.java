2e13e3ceeb04b91dbc06a89951185f3b91eb15fb
Ulrik Pagh Schultz <ups@mmmi.sdu.dk>
2013-03-08 12:05:30 +0100

    refactoring of path gathering, new generated structure

--- Link/SoftwareInterfaceGenerator/unityLinkSoftwareInterfaceGenerator/xtend-gen/dk/sdu/mmmi/embedix/generator/ROSmsgCompiler.java
+++ Link/SoftwareInterfaceGenerator/unityLinkSoftwareInterfaceGenerator/xtend-gen/dk/sdu/mmmi/embedix/generator/ROSmsgCompiler.java
@@ -1,6 +1,7 @@
 package dk.sdu.mmmi.embedix.generator;
 
 import com.google.common.base.Objects;
+import dk.sdu.mmmi.embedix.generator.TopicHolder;
 import dk.sdu.mmmi.embedix.generator.Utilities;
 import dk.sdu.mmmi.embedix.ulswig.AddressSpec;
 import dk.sdu.mmmi.embedix.ulswig.Constructor;
@@ -29,16 +30,16 @@ import org.eclipse.xtext.xbase.lib.Functions.Function0;
 public class ROSmsgCompiler {
   private LinkSpec spec;
   
-  private final Map<String,List<String>> writeTopics = new Function0<Map<String,List<String>>>() {
-    public Map<String,List<String>> apply() {
-      HashMap<String,List<String>> _hashMap = new HashMap<String,List<String>>();
+  private final Map<TopicHolder,List<String>> writeTopics = new Function0<Map<TopicHolder,List<String>>>() {
+    public Map<TopicHolder,List<String>> apply() {
+      HashMap<TopicHolder,List<String>> _hashMap = new HashMap<TopicHolder,List<String>>();
       return _hashMap;
     }
   }.apply();
   
-  private final Map<String,List<String>> readTopics = new Function0<Map<String,List<String>>>() {
-    public Map<String,List<String>> apply() {
-      HashMap<String,List<String>> _hashMap = new HashMap<String,List<String>>();
+  private final Map<TopicHolder,List<String>> readTopics = new Function0<Map<TopicHolder,List<String>>>() {
+    public Map<TopicHolder,List<String>> apply() {
+      HashMap<TopicHolder,List<String>> _hashMap = new HashMap<TopicHolder,List<String>>();
       return _hashMap;
     }
   }.apply();
@@ -53,7 +54,7 @@ public class ROSmsgCompiler {
         for (final Member m : _members) {
           String _name = spec.getName();
           String _name_1 = c.getName();
-          String _join = this.join(_name, _name_1);
+          TopicHolder _join = this.join(_name, _name_1);
           this.expandTopicPath(_join, m, c);
         }
       }
@@ -65,17 +66,22 @@ public class ROSmsgCompiler {
     this.generateMSG(this.readTopics, "R", directory, access);
   }
   
-  public void generateMSG(final Map<String,List<String>> map, final String prefix, final String directory, final IFileSystemAccess access) {
-    Set<Entry<String,List<String>>> _entrySet = map.entrySet();
-    for (final Entry<String,List<String>> e : _entrySet) {
+  public void generateMSG(final Map<TopicHolder,List<String>> map, final String prefix, final String directory, final IFileSystemAccess access) {
+    Set<Entry<TopicHolder,List<String>>> _entrySet = map.entrySet();
+    for (final Entry<TopicHolder,List<String>> e : _entrySet) {
       String _plus = (directory + "/");
-      String _plus_1 = (_plus + prefix);
-      String _key = e.getKey();
-      String _plus_2 = (_plus_1 + _key);
-      String _plus_3 = (_plus_2 + ".msg");
+      TopicHolder _key = e.getKey();
+      String _baseName = _key.getBaseName();
+      String _plus_1 = (_plus + _baseName);
+      String _plus_2 = (_plus_1 + "/msg/");
+      String _plus_3 = (_plus_2 + prefix);
+      TopicHolder _key_1 = e.getKey();
+      String _rosName = _key_1.getRosName();
+      String _plus_4 = (_plus_3 + _rosName);
+      String _plus_5 = (_plus_4 + ".msg");
       List<String> _value = e.getValue();
       CharSequence _generateMSGelements = this.generateMSGelements(_value);
-      access.generateFile(_plus_3, _generateMSGelements);
+      access.generateFile(_plus_5, _generateMSGelements);
     }
   }
   
@@ -91,21 +97,21 @@ public class ROSmsgCompiler {
     return _builder;
   }
   
-  protected void _expandTopicPath(final String base, final Member member, final Constructor context) {
+  protected void _expandTopicPath(final TopicHolder base, final Member member, final Constructor context) {
   }
   
-  protected void _expandTopicPath(final String base, final Expansion expansion, final Constructor context) {
+  protected void _expandTopicPath(final TopicHolder base, final Expansion expansion, final Constructor context) {
     Constructor _constructor = expansion.getConstructor();
     EList<Member> _members = _constructor.getMembers();
     for (final Member m : _members) {
       String _name = expansion.getName();
-      String _join = this.join(base, _name);
+      TopicHolder _join = this.join(base, _name);
       Constructor _constructor_1 = expansion.getConstructor();
       this.expandTopicPath(_join, m, _constructor_1);
     }
   }
   
-  protected void _expandTopicPath(final String base, final Instantiation instant, final Constructor context) {
+  protected void _expandTopicPath(final TopicHolder base, final Instantiation instant, final Constructor context) {
     try {
       String _kind = instant.getKind();
       boolean _equals = _kind.equals("READ");
@@ -145,7 +151,7 @@ public class ROSmsgCompiler {
     return false;
   }
   
-  protected void _expandTopicPath(final String base, final Grouping group, final Constructor context) {
+  protected void _expandTopicPath(final TopicHolder base, final Grouping group, final Constructor context) {
     ArrayList<String> _arrayList = new ArrayList<String>();
     final List<String> expansion = _arrayList;
     EList<GroupElement> _elements = group.getElements();
@@ -155,12 +161,12 @@ public class ROSmsgCompiler {
     }
     for (final String e_1 : expansion) {
       String _name = group.getName();
-      String _join = this.join(base, _name);
+      TopicHolder _join = this.join(base, _name);
       this.putIntoList(this.writeTopics, _join, e_1);
     }
   }
   
-  public boolean putIntoList(final Map<String,List<String>> map, final String base, final String name) {
+  public boolean putIntoList(final Map<TopicHolder,List<String>> map, final TopicHolder base, final String name) {
     boolean _xblockexpression = false;
     {
       List<String> _get = map.get(base);
@@ -176,13 +182,26 @@ public class ROSmsgCompiler {
     return _xblockexpression;
   }
   
-  public String join(final String base, final String extend) {
-    String _plus = (base + "_");
+  public TopicHolder join(final String base, final String extend) {
+    String _plus = (base + ".");
     String _plus_1 = (_plus + extend);
-    return _plus_1;
+    TopicHolder _topicHolder = new TopicHolder(base, extend, _plus_1);
+    return _topicHolder;
   }
   
-  public void expandTopicPath(final String base, final Member expansion, final Constructor context) {
+  public TopicHolder join(final TopicHolder holder, final String extend) {
+    String _baseName = holder.getBaseName();
+    String _rosName = holder.getRosName();
+    String _plus = (_rosName + "_");
+    String _plus_1 = (_plus + extend);
+    String _pythonName = holder.getPythonName();
+    String _plus_2 = (_pythonName + "_");
+    String _plus_3 = (_plus_2 + extend);
+    TopicHolder _topicHolder = new TopicHolder(_baseName, _plus_1, _plus_3);
+    return _topicHolder;
+  }
+  
+  public void expandTopicPath(final TopicHolder base, final Member expansion, final Constructor context) {
     if (expansion instanceof Expansion) {
       _expandTopicPath(base, (Expansion)expansion, context);
       return;
